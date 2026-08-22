4273d0b8b129d0072efe0f8de62deec1b19e7887
SpriteForge <admin@spriteforge.xyz>
2026-08-22 18:38:31 +0200

    Fix library import dialog layout

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { processPixelGrid, quantizePalette, snapToGrid } from "/pixel-grid-core.js";
 import { characterCreatorMarkup, setupCharacterCreator } from "/character-creator.js?v=26";
 import { assetGeneratorMarkup, setupAssetGenerator } from "/asset-generator.js?v=10";
-import { appStudioMarkup, setupAppStudio } from "/app-studio.js?v=101";
+import { appStudioMarkup, setupAppStudio } from "/app-studio.js?v=102";
 import "/cost-display.js?v=6";
 import "/perspective-assets.js?v=1";
 
