2efa5d782b149133ba792598f897c6b936a0f00c
SpriteForge <admin@spriteforge.xyz>
2026-08-23 19:25:22 +0200

    Rename manual editor to Pixel Editor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { processPixelGrid, quantizePalette, snapToGrid } from "/pixel-grid-core.js";
 import { characterCreatorMarkup, setupCharacterCreator } from "/character-creator.js?v=26";
 import { assetGeneratorMarkup, setupAssetGenerator } from "/asset-generator.js?v=11";
-import { appStudioMarkup, setupAppStudio } from "/app-studio.js?v=110";
+import { appStudioMarkup, setupAppStudio } from "/app-studio.js?v=111";
 import "/cost-display.js?v=6";
 import "/perspective-assets.js?v=1";
 
