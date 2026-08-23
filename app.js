b1a77a6d9eacc0cc0517a049d7be68a7203ace8a
SpriteForge <admin@spriteforge.xyz>
2026-08-23 20:31:35 +0200

    Make animate again start a new generation

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { processPixelGrid, quantizePalette, snapToGrid } from "/pixel-grid-core.js";
 import { characterCreatorMarkup, setupCharacterCreator } from "/character-creator.js?v=29";
 import { assetGeneratorMarkup, setupAssetGenerator } from "/asset-generator.js?v=13";
-import { appStudioMarkup, setupAppStudio } from "/app-studio.js?v=115";
+import { appStudioMarkup, setupAppStudio } from "/app-studio.js?v=116";
 import "/cost-display.js?v=6";
 import "/perspective-assets.js?v=1";
 
